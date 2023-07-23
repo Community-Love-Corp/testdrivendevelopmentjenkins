e5e627ad4f8f64d348f81a1592d2dd7c2a1ed3b5
Jay Sarna <jyotirmay.sarna@gmail.com>
2023-07-24 00:06:10 +1200

    try #3 - Added some sample tests to show CI/CD possible with test automation

--- src/test/java/com/blessing/tdd4/Tdd4ApplicationTests.java
+++ src/test/java/com/blessing/tdd4/Tdd4ApplicationTests.java
@@ -2,12 +2,20 @@ package com.blessing.tdd4;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import io.micrometer.core.annotation.TimedSet;
 
 @SpringBootTest
 class Tdd4ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void samplejunittest(){
+		Assert.isTrue(true, "msg if false");
 	}
 
 }
